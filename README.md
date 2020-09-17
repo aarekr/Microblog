94e6a972c0c88d3e7afe29fdaadfff5fb8b2d3db
Aare <aare212@hotmail.com>
2020-09-17 23:12:33 +0300

    Heroku & Postgres setup

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # Microblog
 Blog app based on Miguel Grinberg's [Flask Mega Tutorial](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world)
 
-#### Languages
-The app can be used in English and Spanish. Change your browser language settings or choose the right return xx line in app/config.py.
-
-Translations are in .po files and Python produces .mo files.
+### Languages
+The app can be used in English and Spanish. Change your browser language settings or choose the right return xx line in app/config.py. Translations are in .po files and Python produces .mo files.
 
 Update your translations with these 3 commands:
 1. pybabel extract -F babel.cfg -k _l -o messages.pot .
 2. pybabel update -i messages.pot -d app/translations
 3. pybabel compile -d app/translations
 
-#### Installing requirements
+### Installing requirements
 pip install -r requirements.txt
 
-#### Database update
+### Database update
 1. flask db migrate -m "description of the change made"
 2. flask db upgrade
--- app/__init__.py
+++ app/__init__.py
@@ -63,6 +63,30 @@ def create_app(config_class=Config):
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
 
+    if not app.debug and not app.testing:
+        # ...
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Microblog startup')
+
+    return app
+
+
 @babel.localeselector
 def get_locale(): # choose the app language here:
     # return request.accept_languages.best_match(app.config['LANGUAGES'])
